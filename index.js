aa7b0f22a28f8fe9dab348a174c99de164aeef14
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-24 18:01:16 +0530

    conditions

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ if (light == "red") {
   console.log("invalid input");//if input in invaild o/p:- invalid input
 }
 
+var temp="25";
+if (temp=="0") {
+  console.log("its freesing cold");//if temp=0 o/p:-its freesing cold
+  
+}else if(temp==15){
+  console.log("its a cool day");//if temp=15 o/p:-its a cool day
+}else if(temp==25){
+  console.log("its a pleasent day");//if temp=25 o/p:-its a pleasent day
+}else{
+  console.log("unknown weather");//if temp=unknown o/p:-unknown weather
+  
+}
