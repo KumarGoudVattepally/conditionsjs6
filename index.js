ab55846f70c33b0208e871f0ab4015d2d072aa27
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-24 18:13:34 +0530

    conditons

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 var light = "green";
 if (light == "red") {
-  console.log("stop");//if light is red o/p:-stop
-} else if (light== "yellow") {
-  console.log("get ready to move");//if light is yellow o/p:-get ready to move
+  console.log("stop"); //if light is red o/p:-stop
+} else if (light == "yellow") {
+  console.log("get ready to move"); //if light is yellow o/p:-get ready to move
 } else if (light == "green") {
-  console.log("go");//if light is green o/p:-g0
+  console.log("go"); //if light is green o/p:-g0
 } else {
-  console.log("invalid input");//if input in invaild o/p:- invalid input
+  console.log("invalid input"); //if input in invaild o/p:- invalid input
 }
 
-var temp="25";
-if (temp=="0") {
-  console.log("its freesing cold");//if temp=0 o/p:-its freesing cold
-  
-}else if(temp==15){
-  console.log("its a cool day");//if temp=15 o/p:-its a cool day
-}else if(temp==25){
-  console.log("its a pleasent day");//if temp=25 o/p:-its a pleasent day
-}else{
-  console.log("unknown weather");//if temp=unknown o/p:-unknown weather
-  
+var temp = "25";
+if (temp == "0") {
+  console.log("its freesing cold"); //if temp=0 o/p:-its freesing cold
+} else if (temp == 15) {
+  console.log("its a cool day"); //if temp=15 o/p:-its a cool day
+} else if (temp == 25) {
+  console.log("its a pleasent day"); //if temp=25 o/p:-its a pleasent day
+} else {
+  console.log("unknown weather"); //if temp=unknown o/p:-unknown weather
+}
+var day = "7";
+if (day == 1) {
+  console.log("today is monday"); //If the number is 1, o/p:- "Today is Monday."
+} else if (day == 2) {
+  console.log("today is tuesday"); //If the number is 2, o/p:- "Today is tuesday."
+} else if (day == 3) {
+  console.log("today is wednesday"); //If the number is 3, o/p:- "Today is wednesday."
+} else if (day == 4) {
+  console.log("today is thursday"); //If the number is 4, o/p:- "Today is thurday."
+} else if (day == 5) {
+  console.log("today is friday"); //If the number is 5, o/p:- "Today is friday."
+} else if (day == 6) {
+  console.log("today is saturday"); //If the number is 6, o/p:- "Today is saturday"
+} else if (day == 7) {
+  console.log("today is sunday"); //If the number is 7, o/p:- "Today is sunday"
+} else {
+  console.log("invaild day number"); //If the number is between 0 to 7, o/p:- "invaild day number"
 }
