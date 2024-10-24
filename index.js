6bc1d516bb52a232821e36fb6a634764b67effd6
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-24 17:41:53 +0530

    cinditions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var light = "green";
 if (light == "red") {
-  console.log("stop");
+  console.log("stop");//if light is red o/p:-stop
 } else if (light== "yellow") {
-  console.log("get ready to move");
+  console.log("get ready to move");//if light is yellow o/p:-get ready to move
 } else if (light == "green") {
-  console.log("go");
+  console.log("go");//if light is green o/p:-g0
 } else {
-  console.log("invalid input");
+  console.log("invalid input");//if input in invaild o/p:- invalid input
 }
 
