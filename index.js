680d7f59aa72f4244b13aceda44633677f7746aa
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-24 17:31:17 +0530

    js6

---
+++ index.js
@@ -0,0 +1,11 @@
+var light = red;
+if (light == red) {
+  console.log("stop");
+} else if (light == yellow);
+{
+  console.log("get ready to move");
+}elseif (light == yellow);
+{
+  console.log("get ready to move");
+}
+
---
+++ jsclass.js
@@ -0,0 +1,23 @@
+// var bike = true;
+// var gamecard = true;
+// if (bike == true) {
+//   if (gamecard == true) {
+//     console.log("played games");
+//   } else {
+//     console.log("just go and watch");
+//   }
+// } else {
+//   console.log("no bike, no mall, no games");
+// }
+var user = "client";
+if (user == "admin") {
+  console.log("admin is logged");
+} else if (user == "deliver_boy") {
+  console.log("delivery boy is logged");
+} else if (user == "res-ower") {
+  console.log(" rea_ower boyis logged");
+} else if (user == "client") {
+  console.log("client is logged");
+} else {
+  console.log("create new ac");
+}
